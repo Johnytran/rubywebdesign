38333a3ce6376779b0f574280a3b3c3c9fe35400
andy <tuananhasp@gmail.com>
2022-04-08 22:20:24 +1000

    css home

--- js/main.js
+++ js/main.js
@@ -1,23 +1,9 @@
-const app = document.getElementById('app')
-
-const myRand = () => {
-  let r = 50
-  while (40 < r && r < 60) {
-    r = Math.random() * 100
-  }
-  return r
-}
-
-for (let i = 0; i < 50; i++) {
-  const delay = Math.random() + 's';
-  const el = document.createElement('img')
-  el.src            = 'images/star.svg'
-  el.className      = 'glitter-star'
-  el.style.top      = myRand() + '%'
-  el.style.left     = myRand() + '%'
-  el.style.animationDelay       = delay
-  el.style.msAnimationDelay     = delay
-  el.style.webkitAnimationDelay = delay
-  el.style.monAnimationDelay    = delay
-  app.appendChild(el)
-}
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
